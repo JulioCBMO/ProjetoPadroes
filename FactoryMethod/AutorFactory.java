8378b20ba6526f4aae7e6ad2da3d7e5bb5f78317
FELIPE MACEDO <144855211+felipemcd@users.noreply.github.com>
2026-06-27 21:49:33 +0000

    refactor: remove classes legadas de Autor e Revisor

--- FactoryMethod/AutorFactory.java
+++
@@ -1,8 +0,0 @@
-package FactoryMethod;
-
-public class AutorFactory implements UsuarioFactory {
-    @Override
-    public Usuario criar(String email, String senha, String instituicao) {
-        return new Autor(email, senha, instituicao);
-    }
-}
--- FactoryMethod/RevisorFactory.java
+++
@@ -1,8 +0,0 @@
-package FactoryMethod;
-
-public class RevisorFactory  implements UsuarioFactory {
-    @Override
-    public Usuario criar(String email, String senha, String instituicao) {
-        return new Revisor(email, senha, instituicao);
-    }
-}
